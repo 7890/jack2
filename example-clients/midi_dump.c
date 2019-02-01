b160dac4bd9f9a3db4fb70204d1bfec24b13a4ca
Thomas Brand <tom@trellis.ch>
2019-02-01 08:44:52 +0000

    exclude signal handler

--- example-clients/midi_dump.c
+++ example-clients/midi_dump.c
@@ -109,10 +109,12 @@ process (jack_nframes_t frames, void* arg)
 	return 0;
 }
 
+#ifndef WIN32
 static void wearedone(int sig) {
 	fprintf(stderr, "Shutting down\n");
 	keeprunning = 0;
 }
+#endif
 
 static void usage (int status) {
 	printf ("jack_midi_dump - JACK MIDI Monitor.\n\n");

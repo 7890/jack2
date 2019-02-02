0123b48d2883705676449894e99c7c56b2a8556f
Thomas Brand <tom@trellis.ch>
2019-02-02 23:23:34 +0000

    swap order

--- common/JackAPI.cpp
+++ common/JackAPI.cpp
@@ -17,7 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 
 */
-
+#define __STDC_FORMAT_MACROS 1
 #include "JackClient.h"
 #include "JackError.h"
 #include "JackGraphManager.h"
@@ -27,7 +27,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
 #include "JackTime.h"
 #include "JackPortType.h"
 #include <math.h>
-#define __STDC_FORMAT_MACROS 1
 #include <inttypes.h>
 
 using namespace Jack;
